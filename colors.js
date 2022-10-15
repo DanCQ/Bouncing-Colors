574ce2411ead6c60334d5bb199d40ef76e4150db
Dany Cervantes <danycervantesq@gmail.com>
2022-10-15 00:22:14 -0400

    Update colors.js
    Added touch/click functionality to the minus and plus signs.

--- colors.js
+++ colors.js
@@ -63,26 +63,6 @@ function flashingWords() {
         name.style.textShadow = "";
     }
 
-/*
-    let play = setInterval(
-        function() {
-            flashColors() 
-    }, 6000); //runs funtion every 6 seconds
-
-    center.onclick = function() {
-
-        if (middle.innerHTML == "pause") {
-            clearInterval(play);
-            middle.innerHTML = "play";
-
-        } else if (middle.innerHTML == "play") {
-            flashColors();
-            middle.innerHTML = "pause";
-            play = setInterval(function () { flashColors() }, 6000);
-        }
-    }
-*/
-
     function flashColors() {
         on(upper); //starts
         setTimeout(function() { 
@@ -104,40 +84,47 @@ function flashingWords() {
         }, 4000); //starts
     }
     flashColors();
-    play = setInterval(flashColors, 6000); //runs funtion every 6 seconds
+    setInterval(flashColors, 6000); //runs funtion every 6 seconds
 }
 
 
 //cycle background color
 function backgroundColors () {
     
-    let index = randomRange(0, 140); //starts function with a random color in the array
+    let i = randomRange(0, 140); //starts function with a random color in the array
+    let j = randomRange(0, 140); //starts function with a random color in the array
     let lowerHalf = document.querySelector(".lower-half");
     let upperHalf = document.querySelector(".upper-half");
 
-    let play = setInterval( 
-        function () { 
-            color() 
-    }, 5000); //automatically changes color every 5 seconds
+    let lowerLeft = document.querySelector(".lower-left");
+    let lowerRight = document.querySelector(".lower-right");
+    let upperLeft = document.querySelector(".upper-left");
+    let upperRight = document.querySelector(".upper-right");
+
+    let play = setInterval(function() { color() }, 5000); //automatically changes color every 5 seconds
 
 
     //advances background color array
     function color () {
-        upper.innerHTML = colorArray[index];
-        upperHalf.style.backgroundColor = colorArray[index];
+        upper.innerHTML = colorArray[i];
+        upperHalf.style.backgroundColor = colorArray[i];
+        i++;
 
-        index++;
+        lower.innerHTML = colorArray[j];
+        lowerHalf.style.backgroundColor = colorArray[j];
+        j++;
+        
         reset();
-
-        lower.innerHTML = colorArray[index];
-        lowerHalf.style.backgroundColor = colorArray[index];
     }
     color();
     
     //resets index if array max is reached ( >= prevents undefined from being displayed )
     function reset () {
-        if (index >= colorArray.length) {
-            index = 0;
+        if (i >= colorArray.length) {
+            i = 0;
+        }
+        if (j >= colorArray.length) {
+            j = 0;
         }
     }
 
@@ -153,6 +140,59 @@ function backgroundColors () {
             play = setInterval(function () { color() }, 5000);
         }
     }
+
+    lowerLeft.onclick = function() {
+        clearInterval(play);
+        middle.innerHTML = "play";
+
+        j--;
+        if(j == -1) {
+            j = 140;
+        }
+
+        lower.innerHTML = colorArray[j];
+        lowerHalf.style.backgroundColor = colorArray[j];
+    }
+
+    lowerRight.onclick = function() {
+        clearInterval(play);
+        middle.innerHTML = "play";
+
+        j++;
+        if(j == 141) {
+            j = 0;
+        }
+
+        lower.innerHTML = colorArray[j];
+        lowerHalf.style.backgroundColor = colorArray[j];
+    }
+
+    upperLeft.onclick = function() {
+        clearInterval(play);
+        middle.innerHTML = "play";
+
+        i--;
+        if(i == -1) {
+            i = 140;
+        }
+
+        upper.innerHTML = colorArray[i];
+        upperHalf.style.backgroundColor = colorArray[i];
+    }
+
+    upperRight.onclick = function() {
+        clearInterval(play);
+        middle.innerHTML = "play";
+
+        i++;
+        if(i == 141) {
+            i = 0;
+        }
+
+        upper.innerHTML = colorArray[i];
+        upperHalf.style.backgroundColor = colorArray[i];
+    }
+
 
 }
 
